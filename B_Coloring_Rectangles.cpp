fe71ab27c3e0daa76452f3d5eb5fca1df1108fe0
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-09-26 22:40:22 +0530

    26th aug

---
+++ B_Coloring_Rectangles.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main(){
+    int t; 
+    cin>> t; 
+    while(t--){
+        int n, m; 
+        cin>> n>> m; 
+        if(n*m%3== 0){
+            cout<< n*m/3<< endl; 
+        }else{
+            cout<< n*m/3 +1<< endl; 
+        }
+    }
+    return 0; 
+}
