eb1699861b2230a764e393268bd89da3436a167a
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-10-06 21:55:21 +0530

    6oct

---
+++ D_X-Sum.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main(){
+    int t; 
+    cin>> t; 
+    while(t--){
+        int n, m; 
+        cin>> n>> m; 
+        vector<vector<int>> grid(n, vector<int> (m)); 
+        for(int i= 0; i< n; i++){
+            for(int j= 0; j< m; j++){
+                cin>> grid[i][j]; 
+            }
+        }
+        int ans= INT_MIN; 
+        for(int i= 0; i< n; i++){
+            for(int j= 0; j< m; j++){
+                int currSum= 0; 
+                int currx= i; 
+                int curry= j; 
+                while(currx>= 0 && curry>= 0 && currx< n && curry< m){
+                    currSum+= grid[currx][curry]; 
+                    currx--; 
+                    curry--; 
+                }
+                currx= i; 
+                curry= j; 
+                while(currx>= 0 && curry>= 0 && currx< n && curry< m){
+                    currSum+= grid[currx][curry]; 
+                    currx++; 
+                    curry--; 
+                }
+                currx= i; 
+                curry= j; 
+                while(currx>= 0 && curry>= 0 && currx< n && curry< m){
+                    currSum+= grid[currx][curry]; 
+                    currx--; 
+                    curry++; 
+                }
+                currx= i; 
+                curry= j; 
+                while(currx>= 0 && curry>= 0 && currx< n && curry< m){
+                    currSum+= grid[currx][curry]; 
+                    currx++; 
+                    curry++; 
+                }
+                currSum-= grid[i][j]*3; 
+                ans= max(ans, currSum); 
+            }
+            
+        }
+        cout<< ans<< endl; 
+    }
+    return 0; 
+}
