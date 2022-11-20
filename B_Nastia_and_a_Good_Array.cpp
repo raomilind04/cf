f73ccad171fe32218bc64dc1bb86b4e6676a40fc
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-11-20 23:38:24 +0530

    20th NOV

--- B_Nastia_and_a_Good_Array.cpp
+++ B_Nastia_and_a_Good_Array.cpp
@@ -3,7 +3,48 @@ using namespace std;
 typedef long long ll;
 
 void solve(){
-    cout<< "HLELLO"<< endl; 
+    int n; 
+    char c; 
+    cin>> n>> c; 
+    string s; 
+    cin>> s; 
+    int count= 0; 
+    bool flag= true; 
+    for(auto it: s){
+        if(it!= c){
+            flag= false; 
+        }
+    }
+    if(!flag){
+        cout<< 0<< endl; 
+        return ; 
+    }
+    vector<int> nums; 
+    for(int i= 1; i< n+1; i++){
+        flag= true; 
+        int j= i; 
+        for(; j< n+1; j+= i){
+            flag&= (s[j-1]== c); 
+        }
+        if(flag){
+            nums.push_back(j); 
+            count= 1; 
+            break; 
+        }
+    }
+    if(!flag){
+        nums.push_back(n); 
+        nums.push_back(n-1); 
+        count= 2; 
+    }
+    cout<< count<< endl; 
+    for(auto it: nums){
+        cout<< it<< " "; 
+    }
+    cout<< endl; 
+    return ; 
+
+     
 }
 
 int main(){
--- C_Make_Them_Equal.cpp
+++ C_Make_Them_Equal.cpp
@@ -1,14 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std; 
+typedef long long ll;
+
+void solve(){
+    int n; 
+    char c; 
+    cin>> n>> c; 
+    string s; 
+    cin>> s; 
+    int count= 0; 
+    bool flag= true; 
+    for(auto it: s){
+        if(it!= c){
+            flag= false; 
+        }
+    }
+    if(flag){
+        cout<< 0<< endl; 
+        return ; 
+    }
+    vector<int> nums; 
+    for(int i= 1; i< n+1; i++){
+        flag= true; 
+        int j= i; 
+        for(; j< n+1; j++){
+            flag&= (s[j-1]== c); 
+            j+= (i-1); 
+        }
+        if(flag){
+            nums.push_back(i); 
+            count= 1; 
+            break; 
+        }
+    }
+    if(!flag){
+        nums.push_back(n); 
+        nums.push_back(n-1); 
+        count= 2; 
+    }
+    cout<< count<< endl; 
+    for(auto it: nums){
+        cout<< it<< " "; 
+    }
+    cout<< endl; 
+    return ; 
+
+     
+}
 
 int main(){
     int t; 
     cin>> t; 
-    while(t--){
-       vector<int> v; 
-       v.push_back(1); 
-       cout<< v[0]<< endl; 
-
+    while(t){
+        solve(); 
+        t--; 
     }
-    return 0; 
+    return 0;
 }
