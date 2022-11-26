6390978931a384f6c6ed4ed2cc49b45b14177d84
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-11-26 18:40:34 +0530

    CSES 26th NOV

--- CSES/Two_Sets.cpp
+++ CSES/Two_Sets.cpp
@@ -5,13 +5,48 @@ typedef long long ll;
 int main(){
     int n; 
     cin>> n; 
-    ll sum= n*(n-1)/2; 
+    ll sum= n*(n+1)/2; 
     if(sum%2!= 0){
         cout<< "NO"<< endl;
+    }else{
+        cout<< "YES"<< endl; 
+        vector<int> s1; 
+        vector<int> s2; 
+        if(n%2== 0){
+            for(int i= 0; i< n/2; i++){
+                if(i%2== 0){
+                    s1.push_back(i+1); 
+                    s1.push_back(n-i); 
+                }else{
+                    s2.push_back(i+1); 
+                    s2.push_back(n-i); 
+                }
+            }
+        }else{
+            for(int i= 0; i< (n-1)/2; i++){
+                if(i%2== 0){
+                    s1.push_back(i+1); 
+                    s1.push_back(n-i-1); 
+                }else{
+                    s2.push_back(i+1); 
+                    s2.push_back(n-i-1); 
+                }
+            }
+            if(s1.size()> s2.size()){
+                s2.push_back(n); 
+            }else{
+                s1.push_back(n);
+            }
+        }
+        cout<< s1.size()<< endl; 
+        for(int i= 0; i< s1.size(); i++){
+            cout<< s1[i]<< " "; 
+        }
+        cout<< endl; 
+        cout<< s2.size()<< endl; 
+        for(int i= 0; i< s2.size(); i++){
+            cout<< s2[i]<< " "; 
+        }
     }
-    ll target= n/2; 
-    ll temp= 0; 
-    int i= n; 
-    while(sum< )
     return 0; 
 }
