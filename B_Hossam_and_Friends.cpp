3d863911c70a5dcc4cb319be97bf01589a4b3179
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-12-14 23:54:40 +0530

    14th Dec

--- B_Hossam_and_Friends.cpp
+++ B_Hossam_and_Friends.cpp
@@ -3,24 +3,30 @@ using namespace std;
 typedef long long ll;
 
 void solve(){
-    ll n, m; 
-    cin>> n>> m; 
-    ll count= 0; 
-    ll rem= 0; 
-    bool f= true; 
-    ll c= 0; 
-    for(int i= 0; i< m; i++){
-        ll n1; 
-        ll n2; 
-        cin>> n1; 
-        cin>> n2; 
-        int diff= abs(n1-n2);
-        rem+= (n-diff-c); 
-        c++; 
-    }
-    ll total= n*(n+1)/2; 
-    ll ans= total-rem; 
-    cout<< ans<< endl; 
+     int n, m;
+        cin>>n>>m;
+        int u, v;
+        vector<int> r(n, n+1);
+        for(int i=0;i<m;i++) {
+            cin>>u>>v;
+            u--;
+            v--;
+            if(u<v) {
+                r[u]=min(r[u], v);
+            }
+            else {
+                r[v]=min(r[v], u);
+            }
+        }
+        vector<int> b(n);
+        b[n-1]=n-1;
+        ll res=1;
+        for(int i=n-2;i>=0;i--) {
+            b[i]=min(r[i]-1, b[i+1]);
+            res+=(b[i]-i+1);
+        }
+        cout<<res<<endl;
+    return ; 
 }
 
 int main(){
